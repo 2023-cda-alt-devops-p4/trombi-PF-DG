bc7a618c11cfe9ef1a0238e9462ca88d39e7d865
PierreFrs <pierrefraisse90@gmail.com>
2023-09-22 16:00:39 +0200

    feat(Map.tsx): Display info in popup
    Display full infos (added social links and hobbies) in popups. Creation
    of dataMapping function

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,57 +1,41 @@
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import data from "../assets/data/data.json";
+import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 
 import styles from "./Map.module.css"
 
 export const Map = () => {
-  const markers = [
-    {
-      id: 1,
-      firstName: "Yassine",
-      lastName: "El Azzati",
-      pictureUrl: "https://source.unsplash.com/random/300×300",
-      location: "Arras",
-      coordinates: ["48.86957838298227", "2.347020866604488"],
-      stack: {
-        front: ["Angular"],
-        back: ["Java SpringBoot"],
-      },
-      hobbies: "Jeux vidéo/sports de combat/chats (Nala et Jasmine)",
-      linkedIn: "https://www.linkedin.com/in/yassine-el-azzati-7b5a331b8/",
-      gitHub: "https://github.com/YassineElazzati",
-    },
-    {
-      id: 2,
-      firstName: "Yassine",
-      lastName: "El Azzati",
-      pictureUrl: "https://source.unsplash.com/random/300×300",
-      location: "Arras",
-      coordinates: ["50.69306256544878", "3.1784134535149517"],
-      stack: {
-        front: ["Angular"],
-        back: ["Java SpringBoot"],
-      },
-      hobbies: "Jeux vidéo/sports de combat/chats (Nala et Jasmine)",
-      linkedIn: "https://www.linkedin.com/in/yassine-el-azzati-7b5a331b8/",
-      gitHub: "https://github.com/YassineElazzati",
-    },
-  ];
+  const markers = data;
 
-  return (
+    const dataMapping = (dataToMap: string[]) => {
+        const mappedData = dataToMap.map((data: string) => {
+            return data
+        })
+        return mappedData
+    }
+
+    return (
     <MapContainer center={[49.6205, 2.9373]} zoom={8} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {markers.map((marker: any) => (
-        <Marker position={marker.coordinates}>
+        <Marker key={marker.id} position={marker.coordinates}>
           <Popup>
             <div>
-                <img src={marker.pictureUrl} alt="" className={styles.popImage} />
+                <img src={marker.pictureUrl} alt={`picture of ${marker.firstName} ${marker.lastName}`} className={styles.popImage} />
                 <h4>{marker.firstName} {marker.lastName}</h4>
-                {marker.stack.front.map(frontLang => (
-                    <p>{frontLang}</p>
-                ))}
+                <div className="language-container">
+                    <p key={marker.id + "front"}>{dataMapping(marker.stack.front)}</p>
+                    <p key={marker.id + "back"}>{dataMapping(marker.stack.back)}</p>
+                </div>
+                <p key={marker.id + "hobbies"}>{dataMapping(marker.hobbies)}</p>
+                <div className="social-icons-container">
+                    <a href={marker.linkedIn} target="_blank"><AiFillLinkedin /></a>
+                    <a href={marker.gitHub} target="_blank"><AiFillGithub /></a>
+                </div>
             </div>
           </Popup>
         </Marker>
